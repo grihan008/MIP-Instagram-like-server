60c08dabb0ff8a7316a1ee317892a285ef08d88b
Grigorii Burdin <birdingri@gmail.com>
2017-03-01 09:10:46 +0200

    fix

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var app = express();
-var cloudinary = require('cloudinary');
+// var cloudinary = require('cloudinary');
 
 
-cloudinary.config({ 
-        cloud_name: 'disvux5pl', 
-        api_key: '229163247356667', 
-        api_secret: 'D1ahx96rsqHAwLFZIhYy6cuIVwM' 
-    });
+// cloudinary.config({ 
+//         cloud_name: 'disvux5pl', 
+//         api_key: '229163247356667', 
+//         api_secret: 'D1ahx96rsqHAwLFZIhYy6cuIVwM' 
+//     });
 // You can store key-value pairs in express, here we store the port setting
 app.set('port', (process.env.PORT || 3000));
 
 // bodyParser needs to be configured for parsing JSON from HTTP body
 app.use(bodyParser.json());
 app.use(cors());
-app.options('*', cors());
+
 // Simple hello world route
 app.get('/', function(req, res) {
     res.send("Hello world");
@@ -203,11 +203,11 @@ app.post('/addFollow', function(req,res){
     });
 });
 
-app.post('/upload', function(req,res){
-    cloudinary.uploader.upload(req.file, function(result) { 
-      res.json(result);
-    });
-});
+// app.post('/upload', function(req,res){
+//     cloudinary.uploader.upload(req.file, function(result) { 
+//       res.json(result);
+//     });
+// });
 
 app.get('/users', function(req,res){
     res.json(users);
